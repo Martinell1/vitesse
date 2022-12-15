e3a8050094a62400c00b8317ea453f7e3805afc7
宵宫 <1029129827@qq.com>
2022-12-16 00:04:49 +0800

    Create index.vue

---
+++ pages/neko/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { data, refresh } = await useAsyncData(
+  'neko',
+  () => $fetch<{
+    result: Array<any>
+  }>('https://nekos.best/api/v2/neko'),
+)
+</script>
+
+<template>
+  <div>
+    <button @click="refresh()">
+      刷新
+    </button>
+    <img :src="data?.result[0].url" class="w-[400px]">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
